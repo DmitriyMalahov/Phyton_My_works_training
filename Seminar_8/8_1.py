a63234d1a4b09696cc6ddc852818ae949ccd8b6d
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-02-27 21:00:13 +0300

    comleted Task 8_1.py

--- Seminar_8/8_1.py
+++ Seminar_8/8_1.py
@@ -19,40 +19,44 @@ def print_all_contacts():
         for line in data:
             print(line)
 
-def find_contact(find_element):
-        with open('phone_book.txt', 'r', encoding='utf8') as data:
-            for line in data:
-                if find_element in line:
-                    print(line)
 
-def add_contact(add_element):
-     with open('phone_book.txt', 'a', encoding='utf8') as data:
-          data.write(add_element +'\n')
+def find_contact(find_element):
+    with open('phone_book.txt', 'r', encoding='utf8') as data:
+        for line in data:
+            if find_element in line:
+                print(line)
+
+
+def add_contact(new_contact):
+    with open('phone_book.txt', 'a', encoding='utf8') as data:
+        data.write(new_contact + '\n')
+
 
 def exit_program():
     data = open('phone_book.txt', 'r', encoding='utf8')
     data.close()
 
+
 def main_menu(numb):
     if numb == 1:
         print_all_contacts()
-    elif numb == 2:
+    if numb == 2:
         find_element = input('Введите Ф.И.О. либо номер телефона для поиска: ')
         find_contact(find_element)
-    elif numb == 3:
-        add_element = input('Введите Ф.И.О. и номер телефона через пробел: ')
-        add_contact(add_element)
-    elif numb == 4:
+    if numb == 3:
+        new_contact = input('Введите Ф.И.О. и номер телефона через пробел: ')
+        add_contact(new_contact)
+    if numb == 4:
         exit_program()
+    else:
+        print('Введено некоректное значение')
+        numb = int(input('Введите:\n 1 - для печати всех контактов;\n 2 - для поиска контакта;\n\
+ 3 - для добавления нового контакта в справочник;\n 4 - для выхода из программы:  '))
+        main_menu(numb)
+
+
+
 
 numb = int(input('Введите:\n 1 - для печати всех контактов;\n 2 - для поиска контакта;\n\
  3 - для добавления нового контакта в справочник;\n 4 - для выхода из программы:  '))
 main_menu(numb)
-
-
-
-# add_contact('Николай Котов +78651790625')
-# print_all_contacts()
-# find_contact('Котов')
-
-
