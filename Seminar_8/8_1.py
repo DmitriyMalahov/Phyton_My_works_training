dace1d4543313ea5c3a6dcf89aafa14b7240ae01
Dmitriy Malakhov <lesnichiy1986@gmail.com>
2023-02-27 22:14:13 +0300

    comleted Task 8_1.py

--- Seminar_8/8_1.py
+++ Seminar_8/8_1.py
@@ -13,50 +13,42 @@
 # 3. Добавить контакт (сразу сохранить)
 # 4. Выход по требованию пользователя
 
+my_file = 'phone_book.txt'
 
-def print_all_contacts():
-    with open('phone_book.txt', 'r', encoding='utf8') as data:
+
+def print_all_contacts(file):
+    with open(file, 'r', encoding='utf8') as data:
         for line in data:
             print(line)
 
 
-def find_contact(find_element):
-    with open('phone_book.txt', 'r', encoding='utf8') as data:
+def find_contact(file, find_element):
+    with open(file, 'r', encoding='utf8') as data:
         for line in data:
             if find_element in line:
                 print(line)
 
 
-def add_contact(new_contact):
-    with open('phone_book.txt', 'a', encoding='utf8') as data:
+def add_contact(file, new_contact):
+    with open(file, 'a', encoding='utf8') as data:
         data.write(new_contact + '\n')
-
-
-def exit_program():
-    data = open('phone_book.txt', 'r', encoding='utf8')
-    data.close()
 
 
 def main_menu(numb):
     if numb == 1:
-        print_all_contacts()
-    if numb == 2:
+        print_all_contacts(my_file)
+    elif numb == 2:
         find_element = input('Введите Ф.И.О. либо номер телефона для поиска: ')
-        find_contact(find_element)
-    if numb == 3:
+        find_contact(my_file, find_element)
+    elif numb == 3:
         new_contact = input('Введите Ф.И.О. и номер телефона через пробел: ')
-        add_contact(new_contact)
+        add_contact(my_file, new_contact)
+
+
+while True:
+    numb = int(input('Введите:\n 1 - для печати всех контактов;\n 2 - для поиска контакта;\n\
+ 3 - для добавления нового контакта в справочник;\n 4 - для выхода из программы:  '))
+    main_menu(numb)
     if numb == 4:
-        exit_program()
-    else:
-        print('Введено некоректное значение')
-        numb = int(input('Введите:\n 1 - для печати всех контактов;\n 2 - для поиска контакта;\n\
- 3 - для добавления нового контакта в справочник;\n 4 - для выхода из программы:  '))
-        main_menu(numb)
+        break
 
-
-
-
-numb = int(input('Введите:\n 1 - для печати всех контактов;\n 2 - для поиска контакта;\n\
- 3 - для добавления нового контакта в справочник;\n 4 - для выхода из программы:  '))
-main_menu(numb)
